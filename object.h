bef9a48dfd35d94e3ca76f2650b5a330b8dc15dd
Qinliang Xue <38639194+XueQinliang@users.noreply.github.com>
2018-05-24 22:49:40 +0800

    Update object.h

--- object.h
+++ object.h
@@ -14,9 +14,15 @@ public:
         this->py=y*oy;
         this->pz=z*oz;
     }
-    void pic(int picx,int picy,int sort,int sizex=2*x,int sizey=4*y){//从素材图片上的位置截取相应的东西
+    void pic(int picx,int picy,int sort,int special=0,int sizex=2*x,int sizey=4*y){//从素材图片上的位置截取相应的东西
         picture=all.copy(picx*2*x,picy*4*y,sizex,sizey);
         this->sort=sort;
+        this->special=special;
+    }
+    void pic(char *p,int sort,int special=0){
+        picture.load(p);
+        this->sort=sort;
+        this->special=special;
     }
     void show(QPainter &paint){//如果物体有z坐标，则用y减两个单位长度来在二维平面表现立体效果
         paint.drawImage(px,py-pz,picture);
@@ -24,12 +30,27 @@ public:
     void show(int ox,int oy,int oz,QPainter &paint){//把东西放置在具体的某位置
         paint.drawImage(ox*x,oy*y-oz*z,picture);
     }
+    bool isbox(){
+        if(special==-1)
+        return true;
+        else return false;
+    }
+    bool tran(){
+        if(special>=1)
+            return true;
+        else return false;
+    }
+    int getspecial(){
+        return special;
+    }
 
     int getsort(){
         return sort;
     }
+    ~object(){}
 protected:
     int pz,sort=0;
+    int special=0;
     QImage picture;
     static QImage all;
 };
